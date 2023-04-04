a93444e691e3f7f057a6ac44bd074ca7f536d103
luckkalpna <107942615+luckkalpna@users.noreply.github.com>
2023-04-04 13:28:21 +0530

    Nested  routes

---
+++ next.js-app/pages/blog/first.js
@@ -0,0 +1,10 @@
+
+function FirstBlog() {
+    return(
+    <h1>
+        First blog Page
+    </h1>
+    )
+}
+
+export default FirstBlog
---
+++ next.js-app/pages/blog/second.js
@@ -0,0 +1,10 @@
+
+function SecondBlog() {
+    return(
+    <h1>
+        Second blog Page
+    </h1>
+    )
+}
+
+export default SecondBlog
