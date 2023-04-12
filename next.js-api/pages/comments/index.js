e5c852d132a1b67435fa1022958ae1988182d936
luckkalpna <107942615+luckkalpna@users.noreply.github.com>
2023-04-12 17:15:30 +0530

    API Delete Request

--- next.js-api/pages/comments/index.js
+++ next.js-api/pages/comments/index.js
@@ -19,8 +19,16 @@ function CommentsPage() {
       }
     })
     const data = await response.json()
-    // setComments([...comments, data])
     console.log(data)
+  }
+
+  const deleteComment = async commentID => {
+    const response = await fetch(`/api/comments/${commentID}`, {
+      method: 'DELETE',
+    })
+    const data = await response.json()
+    console.log(data)
+    fetchComments()
   }
 
   return (
@@ -32,6 +40,7 @@ function CommentsPage() {
         return (
           <div key={comment.id}>
             {comment.id} {comment.text}
+            <button onClick={() => deleteComment(comment.id)}>Delete</button>
           </div>
         )
       })}
